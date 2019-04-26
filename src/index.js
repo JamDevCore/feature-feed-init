677a6f4670c0e855b4f33befe2d8d7b4430bbe5b
James Harding <jamesvitaly1993@gmail.com>
2019-04-26 15:30:10 +0100

    Added shadow to iframe styles

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const styles = `
   right: 0;
   top: 0;
   background: transparent;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
   width: 400px;
   height: 100vh;
   z-index: 1001;
